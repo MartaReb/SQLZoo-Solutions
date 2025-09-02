7ae006102fd599864b432068681324e419d23744
MartaReb <49365894+MartaReb@users.noreply.github.com>
2025-09-02 12:28:20 +0200

    Add 8-15 queries

--- 3_SELECT_from_Nobel.sql
+++ 3_SELECT_from_Nobel.sql
@@ -41,11 +41,59 @@ SELECT *
 FROM nobel		
 WHERE winner IN 
     ('Theodore Roosevelt', 'Woodrow Wilson', 'Jimmy Carter', 'Barack Obama');                                        		
-		
-		
+			
 		
 --7. Show the winners with first name John.		
 		
 SELECT winner		
 FROM nobel		
-WHERE winner LIKE 'John%';		
+WHERE winner LIKE 'John%';	
+
+
+--8. Show the year, subject, and name of physics winners for 1980 together with the chemistry winners for 1984.			
+			
+SELECT yr, subject, winner			
+FROM nobel			
+WHERE (subject = 'physics' AND yr = 1980)			
+OR (subject = 'chemistry' AND yr = 1984);			
+
+--9. Show the year, subject, and name of winners for 1980 excluding chemistry and medicine.	
+	
+SELECT yr, subject, winner	
+FROM nobel	
+WHERE yr = 1980	
+AND subject NOT IN ('chemistry', 'medicine');	
+
+--10. Show year, subject, and name of people who won a 'Medicine' prize in an early year (before 1910, not including 1910) together with winners of a 'Literature' prize in a later year (after 2004, including 2004).		
+		
+SELECT yr, subject, winner		
+FROM nobel		
+WHERE (subject = 'medicine' AND yr < 1910)		
+OR  (subject = 'literature' AND yr >= 2004);		
+
+
+--11. Find all details of the prize won by PETER GRÜNBERG
+
+SELECT *
+FROM nobel
+WHERE winner = 'PETER GRÜNBERG';
+
+--12. Find all details of the prize won by EUGENE O'NEILL
+
+SELECT *
+FROM nobel
+WHERE winner = "EUGENE O'NEILL";
+
+--13. List the winners, year and subject where the winner starts with Sir. Show the the most recent first, then by name order.
+
+SELECT winner, yr, subject 
+FROM nobel
+WHERE  winner LIKE 'Sir%'
+ORDER BY yr DESC, winner;
+
+--14. Show the 1984 winners and subject ordered by subject and winner name; but list chemistry and physics last.
+
+SELECT winner, subject
+FROM nobel
+WHERE yr = 1984
+ORDER BY subject IN ('physics','chemistry'), subject, winner;
