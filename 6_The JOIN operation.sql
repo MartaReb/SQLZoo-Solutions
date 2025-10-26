f08229eff530c0f947b3fce4a7972f088ef0947a
MartaReb <49365894+MartaReb@users.noreply.github.com>
2025-10-26 18:15:22 +0100

    Add 5-8 queries

--- 6_The JOIN operation.sql
+++ 6_The JOIN operation.sql
@@ -25,3 +25,36 @@ FROM game
 JOIN goal		
 ON game.id = goal.matchid		
 WHERE goal.player LIKE 'Mario%';		
+
+--5. Show player, teamid, coach, gtime for all goals scored in the first 10 minutes.
+
+SELECT goal.player, goal.teamid, eteam.coach, goal.gtime
+FROM goal
+JOIN eteam
+ON eteam.id= goal.teamid
+WHERE goal.gtime <= 10;
+
+--6. List the dates of the matches and the name of the team in which 'Fernando Santos' was the team1 coach.
+
+SELECT game.mdate, eteam.teamname
+FROM game
+JOIN eteam
+ON game.team1 = eteam.id
+WHERE eteam.coach = 'Fernando Santos';
+
+--7. List the player for every goal scored in a game where the stadium was 'National Stadium, Warsaw'.
+
+SELECT goal.player
+FROM game
+JOIN goal
+ON game.id = goal.matchid
+WHERE game.stadium = 'National Stadium, Warsaw';
+
+--More difficult questions
+--8. Show the name of all players who scored a goal against Germany.
+
+SELECT DISTINCT goal.player
+FROM game 
+JOIN goal 
+ON goal.matchid = game.id
+WHERE (game.team1 = 'GER' OR game.team2 = 'GER') AND goal.teamid != 'GER';
