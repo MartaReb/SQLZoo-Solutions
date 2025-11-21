46ed13e0a431fa748577a19df2f746a92023cd69
MartaReb <49365894+MartaReb@users.noreply.github.com>
2025-11-21 19:50:02 +0100

    Add 5-7 queries

--- 8_Using Null.sql
+++ 8_Using Null.sql
@@ -4,7 +4,6 @@ SELECT name
 FROM teacher
 WHERE dept IS NULL;
 	
-	
 --2. Note the INNER JOIN misses the teachers with no department and the departments with no teacher.	
 	
 SELECT teacher.name, dept.name	
@@ -12,14 +11,12 @@ FROM teacher
 INNER JOIN dept	
 ON teacher.dept = dept.id;	
 	
-	
 --3. Use a different JOIN so that all teachers are listed.	
 	
 SELECT teacher.name, dept.name	
 FROM teacher	
 LEFT JOIN dept	
-ON teacher.dept = dept.id;	
-	
+ON teacher.dept = dept.id;		
 	
 --4. Use a different JOIN so that all departments are listed.	
 	
@@ -27,3 +24,20 @@ SELECT teacher.name, dept.name
 FROM teacher	
 RIGHT JOIN dept	
 ON teacher.dept = dept.id;	
+
+--5. Use COALESCE to print the mobile number. Use the number '07986 444 2266' if there is no number given. Show teacher name and mobile number or '07986 444 2266'
+
+SELECT name, COALESCE(mobile, '07986 444 2266') 
+FROM teacher;
+
+--6. Print the teacher name and department name. Use the string 'None' where there is no department.
+
+SELECT teacher.name, COALESCE(dept.name, 'None')
+FROM teacher
+LEFT JOIN dept
+ON teacher.dept = dept.id;
+
+--7. Show the number of teachers and the number of mobile phones.
+
+SELECT COUNT(name), COUNT(mobile)
+FROM teacher;
