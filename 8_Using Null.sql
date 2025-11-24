2caa26f03e89036df6877b01e8e9d491944ff9c2
MartaReb <49365894+MartaReb@users.noreply.github.com>
2025-11-24 20:13:39 +0100

    Add 8-10 queries

--- 8_Using Null.sql
+++ 8_Using Null.sql
@@ -41,3 +41,30 @@ ON teacher.dept = dept.id;
 
 SELECT COUNT(name), COUNT(mobile)
 FROM teacher;
+
+--8. Show each department and the number of staff. Use a RIGHT JOIN to ensure that the Engineering department is listed.		
+		
+SELECT dept.name, COUNT(teacher.name)		
+FROM teacher		
+RIGHT JOIN dept		
+ON teacher.dept = dept.id		
+GROUP BY dept.name;				
+		
+--9. Use CASE to show the name of each teacher followed by 'Sci' if the teacher is in dept 1 or 2 and 'Art' otherwise.		
+		
+SELECT name,		
+CASE 
+    WHEN dept IN ('1','2') THEN 'Sci'		
+    ELSE 'Art'		
+END		
+FROM teacher;			
+		
+--10. Use CASE to show the name of each teacher followed by 'Sci' if the teacher is in dept 1 or 2, show 'Art' if the teacher's dept is 3 and 'None' otherwise.		
+		
+SELECT name,		
+CASE 
+    WHEN dept IN ('1','2') THEN 'Sci'		
+    WHEN dept = '3' THEN 'Art'		
+    ELSE 'None'		
+END		
+FROM teacher;		
