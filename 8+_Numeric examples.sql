53868f3926b23e55830887158c62622972cc824b
MartaReb <49365894+MartaReb@users.noreply.github.com>
2025-12-03 19:16:38 +0100

    Add 5-8 queries

--- 8+_Numeric examples.sql
+++ 8+_Numeric examples.sql
@@ -28,3 +28,35 @@ FROM nss
 WHERE question = 'Q22'
 AND subject IN ('(8) Computer Science', '(H) Creative Arts and Design')
 GROUP BY subject;
+
+--5. Show the subject and total number of students who A_STRONGLY_AGREE to question 22 for each of the subjects '(8) Computer Science' and '(H) Creative Arts and Design'.
+
+SELECT subject, SUM(response*A_STRONGLY_AGREE/100)
+FROM nss
+WHERE question = 'Q22'
+    AND subject IN ('(8) Computer Science', '(H) Creative Arts and Design')
+GROUP BY subject;
+
+--6. Show the percentage of students who A_STRONGLY_AGREE to question 22 for the subject '(8) Computer Science' show the same figure for the subject '(H) Creative Arts and Design'. Use the ROUND function to show the percentage without decimal places.
+
+SELECT subject, ROUND(SUM(response*A_STRONGLY_AGREE)/SUM(response),0)
+FROM nss
+WHERE question = 'Q22'
+    AND subject IN ('(8) Computer Science', '(H) Creative Arts and Design')
+GROUP BY subject;
+
+--7. Show the average scores for question 'Q22' for each institution that include 'Manchester' in the name.
+
+SELECT institution, ROUND(SUM(response*score)/SUM(response),0) as score
+FROM nss
+WHERE question='Q22'
+   AND (institution LIKE '%Manchester%')
+GROUP BY institution;
+
+--8. Show the institution, the total sample size and the number of computing students for institutions in Manchester for 'Q01'.
+
+SELECT institution, SUM(sample), SUM(CASE WHEN subject ='(8) Computer Science' THEN sample ELSE 0 END)
+FROM nss
+WHERE institution LIKE '%Manchester%'
+     AND question = 'Q01'
+GROUP BY institution;
